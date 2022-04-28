d1ce78178d6f670576696dcb1bc6e71dc1c98467
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-04-29 02:32:38 +0600

    2nd commit and addeddependencies with index.js file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Emma-john server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Emma-john-server is running at port : ${port}`);
+})
+
+
