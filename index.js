255b9d77a617769f397008b140d235653678d059
Zahid-BM <bmzahidulislam@outlook.com>
2022-07-01 15:30:30 +0600

    Alhamdulillah

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
     res.send('Emma-john server is running');
